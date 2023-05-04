93d41afc46dc662b30fa434d546ef3f4d2f5569b
jordanbkatz <jordanbkatz4@gmail.com>
2023-05-04 18:48:41 -0400

    fixed script indentation

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function insertHiddenCharacters() {
 	var newText = body.editAsText();
 	var count = 0;
 	for (var i = 0; i < text.length; i++) {
-	  var char = text.charAt(i);
-	  if (char == " ") {
-		var index = i + count;
-		newText.insertText(index, "\u034F");
-		count++;
-	  }
+		var char = text.charAt(i);
+		if (char == " ") {
+			var index = i + count;
+			newText.insertText(index, "\u034F");
+			count++;
+		}
 	}
-  }
+}
