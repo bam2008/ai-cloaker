63f91924a90bbc2970a098746246ad116eeebbf8
jordanbkatz <jordanbkatz4@gmail.com>
2023-05-06 20:16:12 -0400

    changed randomness to 5%

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,12 +12,12 @@ const Home: React.FC = () => {
             if (chars[i] !== '\u034F') {
                 temp.push(chars[i]);
             }
-            if (Math.random() < 0.1) {
+            if (Math.random() < 0.05) {
                 temp.push('\u034F');
             }
         }
         setText(temp.join(''));
-    }
+    };
     const decloak = () => {
         let chars = text.split('');
         let temp = [];
@@ -30,7 +30,7 @@ const Home: React.FC = () => {
     };
     const copy = () => {
         navigator.clipboard.writeText(text);
-    }
+    };
     return (
         <div className="home">
             <div className="header">
